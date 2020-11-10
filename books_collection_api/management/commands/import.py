4f9da04960935531c88b43b971b4b855a42ee793
Rafal <hajtpl@gmail.com>
2020-11-10 14:22:32 +0100

    Added importing opinions with command manager

--- books_collection_api/management/commands/import.py
+++ books_collection_api/management/commands/import.py
@@ -1,26 +1,32 @@
 import os
 import csv
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from books_collection_api.models import Author, Category, Book, Opinion
+
+
+BOOKS_FILE = 'ksiazki.csv'
+OPINIONS_FILE = 'opinie.csv'
 
 
 class Command(BaseCommand):
     help = 'Load a data from csv file into the database'
 
-    def add_arguments(self, parser):
+    def add_arguments(self, parser) -> None:
         """ Defining available arguments. """
         parser.add_argument('--path', type=str)
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options) -> None:
         """ Handling command method. """
         path = options['path']
         if path:
             filename = os.path.split(path)[-1]
-            if filename == 'ksiazki.csv':
+            if filename == BOOKS_FILE:
                 self._import_books(filename)
+            elif filename == OPINIONS_FILE:
+                self._import_opinions(filename)
 
-    def _import_books(self, filename):
+    def _import_books(self, filename: str) -> None:
         """ Function which parses csv file with books 
         and inserts them into the database. """
         with open(filename, 'r') as file:
@@ -45,3 +51,29 @@ class Command(BaseCommand):
                     f'Successfully imported {total} books!'))
             else:
                 self.stdout.write(self.style.NOTICE('No new books to import.'))
+
+    def _import_opinions(self, filename: str) -> None:
+        """ Function which parses csv file with opinions 
+        and inserts them into the database. """
+        with open(filename, 'r') as file:
+            total = 0
+            reader = csv.reader(file, delimiter=';')
+            next(reader)
+            for row in reader:
+                isbn = row[0]
+                rate = row[1]
+                description = row[2]
+                try:
+                    book_obj = Book.objects.get(isbn=isbn)
+                except Book.DoesNotExist:
+                    raise CommandError(f'Book with ISBN={isbn} does not exist')
+                opinion_obj, created = Opinion.objects.get_or_create(
+                    rate=rate, description=description, book=book_obj)
+                if created:
+                    total += 1
+            if total:
+                self.stdout.write(self.style.SUCCESS(
+                    f'Successfully imported {total} opinions!'))
+            else:
+                self.stdout.write(self.style.NOTICE(
+                    'No new opinions to import.'))
